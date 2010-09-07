c96963ef6b3aebadb0aaef5410aa1b3ecda54378
McCabe Maxsted <hakushakukun@gmail.com>
2010-09-07 15:32:13 -0700

    Fixed loading of alut lib

--- linden/indra/cmake/OPENAL.cmake
+++ linden/indra/cmake/OPENAL.cmake
@@ -4,6 +4,29 @@ include(Prebuilt)
 
 set(OPENAL ON CACHE BOOL "Enable OpenAL")
 
+# ALUT_LIB
+
+if (WINDOWS)
+set(ALUT_LIB
+ optimized ${ARCH_PREBUILT_DIRS_RELEASE}/alut.lib
+ debug ${ARCH_PREBUILT_DIRS_DEBUG}/alut.lib
+ )
+elseif (DARWIN)
+ find_library( ALUT_LIB
+  NAMES alut.0
+  PATHS ${ARCH_PREBUILT_DIRS_RELEASE}
+  NO_DEFAULT_PATH
+  )
+else (WINDOWS)
+ set(ALUT_LIB alut)
+endif (WINDOWS)
+
+if (NOT ALUT_LIB)
+ message(FATAL_ERROR "ALUT not found!")
+else (NOT ALUT_LIB)
+ # message(STATUS "ALUT found: ${ALUT_LIB}")
+endif (NOT ALUT_LIB)
+
 if (OPENAL)
   if (STANDALONE)
     include(FindPkgConfig)
@@ -18,39 +41,21 @@ if (OPENAL)
     set(OPENAL_LIBRARIES 
 	  optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libOpenAL32.dll.a.lib
 	  debug ${ARCH_PREBUILT_DIRS_DEBUG}/libOpenAL32.dll.a.lib
+	  ${ALUT_LIB}
       )
   elseif (DARWIN)
-    set(OPENAL_LIBRARIES ${ARCH_PREBUILT_DIRS_RELEASE}/libopenal.1.dylib)
+    set(OPENAL_LIBRARIES 
+	  ${ARCH_PREBUILT_DIRS_RELEASE}/libopenal.1.dylib
+	  ${ALUT_LIB}
+	  )
   else (WINDOWS)
 	set(OPENAL_LIBRARIES 
 	  openal
-	  alut
+	  ${ALUT_LIB}
     )
   endif (WINDOWS)
 endif (OPENAL)
 
-# ALUT_LIB
-
-if (WINDOWS)
-set(ALUT_LIB
- optimized ${ARCH_PREBUILT_DIRS_RELEASE}/alut.lib
- debug ${ARCH_PREBUILT_DIRS_DEBUG}/alut.lib
- )
-elseif (DARWIN)
-find_library( ALUT_LIB
-  NAMES alut.0
-  PATHS ${ARCH_PREBUILT_DIRS_RELEASE}
-  NO_DEFAULT_PATH
-  )
-else (WINDOWS)
-set(ALUT_LIB alut)
-endif (WINDOWS)
-
-if (NOT ALUT_LIB)
-message(FATAL_ERROR "ALUT not found!")
-else (NOT ALUT_LIB)
-# message(STATUS "ALUT found: ${ALUT_LIB}")
-endif (NOT ALUT_LIB)
 
 if (OPENAL)
   message(STATUS "Building with OpenAL audio support")
