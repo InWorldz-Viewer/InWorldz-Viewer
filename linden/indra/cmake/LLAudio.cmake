d48375cb71514ef7c4315549087c4467aacb681e
McCabe Maxsted <hakushakukun@gmail.com>
2010-09-07 07:38:37 -0700

    Force openal always on

--- linden/indra/cmake/LLAudio.cmake
+++ linden/indra/cmake/LLAudio.cmake
@@ -1,9 +1,11 @@
 # -*- cmake -*-
 
 include(Audio)
+include(OPENAL)
 
 set(LLAUDIO_INCLUDE_DIRS
     ${LIBS_OPEN_DIR}/llaudio
+	${OPENAL_INCLUDE_DIRS}
     )
 
 set(LLAUDIO_LIBRARIES llaudio ${OPENAL_LIBRARIES})
