6f40618f6578344628c8f3513f2d9d726541b0d3
McCabe Maxsted <hakushakukun@gmail.com>
2010-09-07 10:57:08 -0700

    Fixed up openal detection in cmake (should work cross-platform)

--- linden/indra/cmake/OPENAL.cmake
+++ linden/indra/cmake/OPENAL.cmake
@@ -13,11 +13,44 @@ if (OPENAL)
   else (STANDALONE)
     use_prebuilt_binary(openal-soft)
   endif (STANDALONE)
-  set(OPENAL_LIBRARIES 
-    openal
-    alut
+  
+  if (WINDOWS)
+    set(OPENAL_LIBRARIES 
+	  optimized ${ARCH_PREBUILT_DIRS_RELEASE}/libOpenAL32.dll.a.lib
+	  debug ${ARCH_PREBUILT_DIRS_DEBUG}/libOpenAL32.dll.a.lib
+      )
+  elseif (DARWIN)
+    set(OPENAL_LIBRARIES ${ARCH_PREBUILT_DIRS_RELEASE}/libopenal.1.dylib)
+  else (WINDOWS)
+	set(OPENAL_LIBRARIES 
+	  openal
+	  alut
     )
+  endif (WINDOWS)
 endif (OPENAL)
+
+# ALUT_LIB
+
+if (WINDOWS)
+set(ALUT_LIB
+ optimized ${ARCH_PREBUILT_DIRS_RELEASE}/alut.lib
+ debug ${ARCH_PREBUILT_DIRS_DEBUG}/alut.lib
+ )
+elseif (DARWIN)
+find_library( ALUT_LIB
+  NAMES alut.0
+  PATHS ${ARCH_PREBUILT_DIRS_RELEASE}
+  NO_DEFAULT_PATH
+  )
+else (WINDOWS)
+set(ALUT_LIB alut)
+endif (WINDOWS)
+
+if (NOT ALUT_LIB)
+message(FATAL_ERROR "ALUT not found!")
+else (NOT ALUT_LIB)
+# message(STATUS "ALUT found: ${ALUT_LIB}")
+endif (NOT ALUT_LIB)
 
 if (OPENAL)
   message(STATUS "Building with OpenAL audio support")
