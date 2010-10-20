8dda68c19a075b2e89fe1e5218e5c96a4a28858d
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2010-10-19 22:31:28 -0700

    Changed version to 1.2.0.0

--- linden/indra/llcommon/llversionviewer.h
+++ linden/indra/llcommon/llversionviewer.h
@@ -34,8 +34,8 @@
 #define LL_LLVERSIONVIEWER_H
 
 const S32 LL_VERSION_MAJOR = 1;
-const S32 LL_VERSION_MINOR = 4;
-const S32 LL_VERSION_PATCH = 2;
+const S32 LL_VERSION_MINOR = 2;
+const S32 LL_VERSION_PATCH = 0;
 const S32 LL_VERSION_BUILD = 0;
 
 const char * const LL_CHANNEL = "InWorldz Release";
