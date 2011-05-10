e80ad7c0a83a72c65074d607c664261dbffff1d0
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2011-05-10 15:16:26 -0700

    Added some log info when ao_template.ini can't be found

--- linden/indra/newview/floaterao.cpp
+++ linden/indra/newview/floaterao.cpp
@@ -81,6 +81,11 @@ public:
 						text->insertText(line);
 					}
 					file.close();
+
+					if (line.empty())
+					{
+						llwarns << "Can't open ao_template.ini at << " << mFileName << llendl;
+					}
 			
 					nc->saveIfNeeded();
 				}
