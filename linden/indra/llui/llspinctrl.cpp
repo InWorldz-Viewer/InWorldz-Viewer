d9ff8b8b41cff3b871216e9f48d75a20cd157d90
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2011-07-15 00:52:16 -0700

    Fixed #0001005: build math not working

--- linden/indra/llui/llspinctrl.cpp
+++ linden/indra/llui/llspinctrl.cpp
@@ -49,7 +49,7 @@
 #include "llfocusmgr.h"
 #include "llresmgr.h"
 
-const U32 MAX_STRING_LENGTH = 32;
+const U32 MAX_STRING_LENGTH = 255;
 
 static LLRegisterWidget<LLSpinCtrl> r2("spinner");
  
@@ -123,7 +123,7 @@ LLSpinCtrl::LLSpinCtrl(	const std::string& name, const LLRect& rect, const std::
 	mEditor = new LLLineEditor( std::string("SpinCtrl Editor"), editor_rect, LLStringUtil::null, font,
 								MAX_STRING_LENGTH,
 								&LLSpinCtrl::onEditorCommit, NULL, NULL, this,
-								&LLLineEditor::prevalidateFloat );
+								&LLLineEditor::prevalidateASCII );
 	mEditor->setFollowsLeft();
 	mEditor->setFollowsBottom();
 	mEditor->setFocusReceivedCallback( &LLSpinCtrl::onEditorGainFocus, this );
@@ -132,6 +132,7 @@ LLSpinCtrl::LLSpinCtrl(	const std::string& name, const LLRect& rect, const std::
 	// it's easier to understand
 	//mEditor->setSelectAllonFocusReceived(TRUE);
 	mEditor->setIgnoreTab(TRUE);
+	mEditor->setSelectAllonCommit(FALSE);
 	addChild(mEditor);
 
 	updateEditor();
@@ -292,9 +293,10 @@ void LLSpinCtrl::onEditorCommit( LLUICtrl* caller, void *userdata )
 	LLSpinCtrl* self = (LLSpinCtrl*) userdata;
 	llassert( caller == self->mEditor );
 
-	std::string text = self->mEditor->getText();
-	if( LLLineEditor::postvalidateFloat( text ) )
+	if( self->mEditor->evaluateFloat() )
 	{
+		std::string text = self->mEditor->getText();
+		
 		LLLocale locale(LLLocale::USER_LOCALE);
 		F32 val = (F32) atof(text.c_str());
 
@@ -322,9 +324,17 @@ void LLSpinCtrl::onEditorCommit( LLUICtrl* caller, void *userdata )
 			success = TRUE;
 		}
 	}
-	self->updateEditor();
+	else
+	{
+		// We want to update the editor in case it fails while blanking -- MC
+		success = TRUE;
+	}
 
-	if( !success )
+	if( success )
+	{
+		self->updateEditor();
+	}
+	else
 	{
 		self->reportInvalidData();		
 	}
@@ -461,6 +471,11 @@ BOOL LLSpinCtrl::handleKeyHere(KEY key, MASK mask)
 		if(key == KEY_DOWN)
 		{
 			LLSpinCtrl::onDownBtn(this);
+			return TRUE;
+		}
+		if(key == KEY_RETURN)
+		{
+			forceEditorCommit();
 			return TRUE;
 		}
 	}
