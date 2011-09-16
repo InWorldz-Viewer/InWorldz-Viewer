af8cff6919d3762b654477378e0a51f6effd6769
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2011-09-16 15:42:19 -0700

    Fixed the webkit version in help > about being old. Darn hardcoded versions

--- linden/indra/newview/llfloaterabout.cpp
+++ linden/indra/newview/llfloaterabout.cpp
@@ -250,7 +250,7 @@ LLFloaterAbout::LLFloaterAbout()
 
 	// TODO: Implement media plugin version query
 
-	support.append("Qt Webkit Version: 4.5.2 ");
+	support.append("Qt Webkit Version: 4.7.1 (version number hard-coded)");
 	support.append("\n");
 
 	if (gPacketsIn > 0)
