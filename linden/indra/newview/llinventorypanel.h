87cb3a1d072ebba99474f51b18d50d1423f96344
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2012-02-13 18:33:27 -0700

    Added GPLv2 licensing headers for newly created source files

--- linden/indra/newview/llinventorypanel.h
+++ linden/indra/newview/llinventorypanel.h
@@ -1,5 +1,29 @@
-// Portions of the inventory window compatible with llui.
-// For the rest, see llinventoryview -- MC
+/**
+ * @file llinventorypanel.h
+ * @brief Portions of the inventory window compatible with llui
+ *
+ * Copyright (c) 2012, McCabe Maxsted
+ *
+ * The source code in this file ("Source Code") is provided to you
+ * under the terms of the GNU General Public License, version 2.0
+ * ("GPL"). Terms of the GPL can be found in doc/GPL-license.txt in
+ * this distribution, or online at
+ * http://www.inworldz.com/izwiki/index.php/Viewers:Licenses#GNU_General_Public_License_v2.0
+ *
+ * There are special exceptions to the terms and conditions of the GPL as
+ * it is applied to this Source Code. View the full text of the exception
+ * in the file doc/FLOSS-exception.txt in this software distribution, or
+ * online at
+ * http://www.inworldz.com/izwiki/index.php/Viewers:Licenses#FLOSS_Exception
+ *
+ * By copying, modifying or distributing this software, you acknowledge
+ * that you have read and understood your obligations described above,
+ * and agree to abide by those obligations.
+ *
+ * ALL SOURCE CODE IS PROVIDED "AS IS." THE AUTHOR MAKES NO
+ * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
+ * COMPLETENESS OR PERFORMANCE.
+ */
 
 #ifndef LL_INVENTORYPANEL_H
 #define LL_INVENTORYPANEL_H
