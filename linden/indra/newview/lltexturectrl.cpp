317ea020753ff150bbb9c89db03ddb5d46ef6b76
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2012-04-09 19:36:48 -0700

    Revert part of e509b3d. This broke texture selection in the texture picker

--- linden/indra/newview/lltexturectrl.cpp
+++ linden/indra/newview/lltexturectrl.cpp
@@ -1282,7 +1282,6 @@ void LLTextureCtrl::setImageAssetID( const LLUUID& asset_id )
 	{
 		mImageItemID.setNull();
 		mImageAssetID = asset_id;
-		mDirty = TRUE;
 		LLFloaterTexturePicker* floaterp = (LLFloaterTexturePicker*)mFloaterHandle.get();
 		if( floaterp && getEnabled() )
 		{
