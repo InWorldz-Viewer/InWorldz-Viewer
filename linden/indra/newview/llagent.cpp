c513d7b8bcaac8eecaeae21ecd6368fc75632a86
McCabe Maxsted <McCabe.Maxsted@gmail.com>
2012-09-23 21:02:20 -0700

    Applied resetviewpotentialcrashfix from coolviewer

--- linden/indra/newview/llagent.cpp
+++ linden/indra/newview/llagent.cpp
@@ -516,7 +516,10 @@ void LLAgent::resetView(BOOL reset_camera, BOOL change_camera)
 		}
 
 		// Hide all popup menus
-		gMenuHolder->hideMenus();
+		if (gMenuHolder)
+		{
+			gMenuHolder->hideMenus();
+		}
 	}
 
 	if (change_camera && !gSavedSettings.getBOOL("FreezeTime"))
